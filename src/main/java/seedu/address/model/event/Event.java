ccb9962657ce11f6099e2de2bc95d0bf7197a46e
Dominic Ng Jun Yao <e0958779@u.nus.edu>
2023-10-12 15:10:38 +0800

    Create Event Class

---
+++ src/main/java/seedu/address/model/event/Event.java
@@ -0,0 +1,89 @@
+package seedu.address.model.event;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Objects;
+
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Represents a Event in EventWise.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Event {
+
+    // Identity fields
+    private final Name name;
+    private final Description description;
+    private final Date date;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Event(Name name, Description description, Date date) {
+        requireAllNonNull(name, description, date);
+        this.name = name;
+        this.description = description;
+        this.date = date;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Description getDescription() {
+        return description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Returns true if both events have the same event name.
+     */
+    public boolean isSameEvent(Event otherEvent) {
+        if (otherEvent == this) {
+            return true;
+        }
+
+        return otherEvent != null
+                && otherEvent.getName().equals(getName());
+    }
+
+    /**
+     * Returns true if both events have the same identity and data fields.
+     * This defines a stronger notion of equality between two events.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Event)) {
+            return false;
+        }
+
+        Event otherPerson = (Event) other;
+        return name.equals(otherPerson.name)
+                && description.equals(otherPerson.description)
+                && date.equals(otherPerson.date);
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, description, date);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("name", name)
+                .add("description", description)
+                .add("date", date)
+                .toString();
+    }
+}
