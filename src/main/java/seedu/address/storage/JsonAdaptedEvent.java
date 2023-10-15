1d6845f3915f64dbff22f3acd284b2f0ab84a270
ktzy0305 <kevintoh0305@gmail.com>
2023-10-15 21:22:10 +0800

    Storage: Add JSONAdapted Event

--- src/main/java/seedu/address/storage/JsonAdaptedEvent.java
+++ src/main/java/seedu/address/storage/JsonAdaptedEvent.java
@@ -1,2 +1,72 @@
-package seedu.address.storage;public class JsonAdaptedEvent {
+package seedu.address.storage;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.event.Date;
+import seedu.address.model.event.Description;
+import seedu.address.model.event.Event;
+import seedu.address.model.event.Name;
+
+/**
+ * Jackson-friendly version of {@link Event}.
+ */
+class JsonAdaptedEvent {
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Event's %s field is missing!";
+
+    private final String name;
+    private final String description;
+    private final String date;
+
+    /**
+     * Constructs a {@code JsonAdaptedEvent} with the given event details.
+     */
+    @JsonCreator
+    public JsonAdaptedEvent(@JsonProperty("name") String name,
+                            @JsonProperty("email") String description,
+                            @JsonProperty("date") String date) {
+        this.name = name;
+        this.description = description;
+        this.date = date;
+    }
+
+    /**
+     * Converts a given {@code Event} into this class for Jackson use.
+     */
+    public JsonAdaptedEvent(Event source) {
+        name = source.getName().eventName;
+        description = source.getDescription().eventDesc;
+        date = source.getDate().eventDate;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted person object into the model's {@code Event} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted person.
+     */
+    public Event toModelType() throws IllegalValueException {
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+        }
+        if (!Name.isValidName(name)) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
+        final Name modelName = new Name(name);
+
+        if (description == null) {
+            throw new IllegalValueException(
+                    String.format(MISSING_FIELD_MESSAGE_FORMAT, Description.class.getSimpleName()));
+        }
+
+        final Description modelDescription = new Description(description);
+
+        if (date == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Date.class.getSimpleName()));
+        }
+
+        final Date modelDate = new Date(date);
+
+        return new Event(modelName, modelDescription, modelDate);
+    }
 }
