9b587d17db5c0b76ee72c351ccd480d4b5db556d
ktzy0305 <kevintoh0305@gmail.com>
2023-10-15 23:42:14 +0800

    Unit Test: ViewEventCommandTest and ViewEventCommandParserTest

--- src/test/java/seedu/address/logic/commands/ViewEventCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewEventCommandTest.java
@@ -1,12 +1,16 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.ViewEventCommand.MESSAGE_SUCCESS;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalEvents.ACADEMIC;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EVENT;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_GREATER_THAN_RANGE_EVENT;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.Messages;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -16,8 +20,20 @@ public class ViewEventCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute() {
-        assertCommandFailure(new ViewEventCommand(INDEX_FIRST_EVENT), model,
-                String.format(MESSAGE_SUCCESS, INDEX_FIRST_EVENT.getOneBased(), "Event name"));
+    public void execute_validIndex_withinRange_success() {
+        ViewEventCommand viewEventCommand = new ViewEventCommand(INDEX_FIRST_EVENT);
+        String expectedMessage = String.format(ViewEventCommand.MESSAGE_SUCCESS,
+                INDEX_FIRST_EVENT.getOneBased(), ACADEMIC.getName().eventName);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+
+        assertCommandSuccess(viewEventCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndex_outOfRange_failure() {
+        ViewEventCommand viewEventCommand = new ViewEventCommand(INDEX_GREATER_THAN_RANGE_EVENT);
+        String expectedMessage = String.format(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
+
+        assertCommandFailure(viewEventCommand, model, expectedMessage);
     }
 }
--- src/test/java/seedu/address/logic/parser/ViewEventCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewEventCommandParserTest.java
@@ -1,14 +1,16 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.ViewEventCommand;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_ID;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EVENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_EVENT;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.ViewEventCommand;
 
 public class ViewEventCommandParserTest {
     private ViewEventCommandParser parser = new ViewEventCommandParser();
@@ -20,6 +22,21 @@ public class ViewEventCommandParserTest {
         assertParseSuccess(parser, userInput, expectedCommand);
     }
 
+    @Test
+    public void parse_duplicateIndexPrefix_failure() {
+        String userInput = String.format(" %s%d %s%d",
+                PREFIX_EVENT_ID, INDEX_FIRST_EVENT.getOneBased(),
+                PREFIX_EVENT_ID, INDEX_SECOND_EVENT.getOneBased());
+        String expectedMessage = String.format(Messages.getErrorMessageForDuplicatePrefixes(PREFIX_EVENT_ID));
+        assertParseFailure(parser, userInput, expectedMessage);
+    }
+
+    @Test
+    public void parse_preambleBeforeIndexPrefix_failure() {
+        String userInput = String.format("%s %s%d", "Word", PREFIX_EVENT_ID, INDEX_FIRST_EVENT.getOneBased());
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewEventCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, userInput, expectedMessage);
+    }
 
     @Test
     public void parse_missingCompulsoryField_failure() {
