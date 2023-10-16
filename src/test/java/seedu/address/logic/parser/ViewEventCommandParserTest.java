8080e08e28bd7fabd826cb80d1974277daf6a16b
ktzy0305 <kevintoh0305@gmail.com>
2023-10-16 13:05:03 +0800

    Unit Test: Add UniqueEventListTest

--- src/test/java/seedu/address/logic/parser/ViewEventCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewEventCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_ID;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
@@ -33,7 +34,8 @@ public class ViewEventCommandParserTest {
 
     @Test
     public void parse_preambleBeforeIndexPrefix_failure() {
-        String userInput = String.format("%s %s%d", "Word", PREFIX_EVENT_ID, INDEX_FIRST_EVENT.getOneBased());
+        String userInput = String.format("%s %s%d", PREAMBLE_NON_EMPTY,
+                PREFIX_EVENT_ID, INDEX_FIRST_EVENT.getOneBased());
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewEventCommand.MESSAGE_USAGE);
         assertParseFailure(parser, userInput, expectedMessage);
     }
---
+++ src/test/java/seedu/address/model/event/UniqueEventListTest.java
@@ -0,0 +1,178 @@
+package seedu.address.model.event;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EVENT_DATE_CAREER_FAIR;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EVENT_DESCRIPTION_CAREER_FAIR;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalEvents.FOC;
+import static seedu.address.testutil.TypicalEvents.FSC;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.event.exceptions.DuplicateEventException;
+import seedu.address.model.event.exceptions.EventNotFoundException;
+import seedu.address.testutil.EventBuilder;
+
+public class UniqueEventListTest {
+    private final UniqueEventList uniqueEventList = new UniqueEventList();
+
+    @Test
+    public void contains_nullPerson_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.contains(null));
+    }
+
+    @Test
+    public void contains_eventNotInList_returnsFalse() {
+        assertFalse(uniqueEventList.contains(FSC));
+    }
+
+    @Test
+    public void contains_eventInList_returnsTrue() {
+        uniqueEventList.add(FSC);
+        assertTrue(uniqueEventList.contains(FSC));
+    }
+
+    @Test
+    public void contains_eventWithSameIdentityFieldsInList_returnsTrue() {
+        uniqueEventList.add(FSC);
+        Event editedFsc = new EventBuilder(FSC)
+                .withDescription(VALID_EVENT_DESCRIPTION_CAREER_FAIR)
+                .withDate(VALID_EVENT_DATE_CAREER_FAIR)
+                .build();
+        assertTrue(uniqueEventList.contains(editedFsc));
+    }
+
+    @Test
+    public void add_nullEvent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.add(null));
+    }
+
+    @Test
+    public void add_duplicateEvent_throwsDuplicateEventException() {
+        uniqueEventList.add(FSC);
+        assertThrows(DuplicateEventException.class, () -> uniqueEventList.add(FSC));
+    }
+
+    @Test
+    public void setEvent_nullTargetEvent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.setEvent(null, FSC));
+    }
+
+    @Test
+    public void setEvent_nullEditedEvent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.setEvent(FSC, null));
+    }
+
+    @Test
+    public void setEvent_targetEventNotInList_throwsEventNotFoundException() {
+        assertThrows(EventNotFoundException.class, () -> uniqueEventList.setEvent(FSC, FSC));
+    }
+
+    @Test
+    public void setEvent_editedEventIsSameEvent_success() {
+        uniqueEventList.add(FSC);
+        uniqueEventList.setEvent(FSC, FSC);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        expectedUniqueEventList.add(FSC);
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvent_editedEventHasSameIdentity_success() {
+        uniqueEventList.add(FSC);
+        Event editedFsc = new EventBuilder(FSC)
+                .withDescription(VALID_EVENT_DESCRIPTION_CAREER_FAIR)
+                .withDate(VALID_EVENT_DATE_CAREER_FAIR)
+                .build();
+        uniqueEventList.setEvent(FSC, editedFsc);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        expectedUniqueEventList.add(editedFsc);
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvent_editedEventHasDifferentIdentity_success() {
+        uniqueEventList.add(FSC);
+        uniqueEventList.setEvent(FSC, FOC);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        expectedUniqueEventList.add(FOC);
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvent_editedEventHasNonUniqueIdentity_throwsDuplicateEventException() {
+        uniqueEventList.add(FSC);
+        uniqueEventList.add(FOC);
+        assertThrows(DuplicateEventException.class, () -> uniqueEventList.setEvent(FSC, FOC));
+    }
+
+    @Test
+    public void remove_nullEvent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.remove(null));
+    }
+
+    @Test
+    public void remove_eventDoesNotExist_throwsPersonNotFoundException() {
+        assertThrows(EventNotFoundException.class, () -> uniqueEventList.remove(FSC));
+    }
+
+    @Test
+    public void remove_existingEvent_removesEvent() {
+        uniqueEventList.add(FSC);
+        uniqueEventList.remove(FSC);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvents_nullUniqueEventList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.setEvents((UniqueEventList) null));
+    }
+
+    @Test
+    public void setEvents_uniqueEventList_replacesOwnListWithProvidedUniqueEventList() {
+        uniqueEventList.add(FSC);
+        UniqueEventList expectedUniquePersonList = new UniqueEventList();
+        expectedUniquePersonList.add(FOC);
+        uniqueEventList.setEvents(expectedUniquePersonList);
+        assertEquals(expectedUniquePersonList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvents_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.setEvents((List<Event>) null));
+    }
+
+    @Test
+    public void setEvents_list_replacesOwnListWithProvidedList() {
+        uniqueEventList.add(FSC);
+        List<Event> personList = Collections.singletonList(FOC);
+        uniqueEventList.setEvents(personList);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        expectedUniqueEventList.add(FOC);
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvents_listWithDuplicateEvents_throwsDuplicateEventException() {
+        List<Event> listWithDuplicateEvents = Arrays.asList(FSC, FSC);
+        assertThrows(DuplicateEventException.class, () -> uniqueEventList.setEvents(listWithDuplicateEvents));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+                -> uniqueEventList.asUnmodifiableObservableList().remove(0));
+    }
+
+    @Test
+    public void toStringMethod() {
+        assertEquals(uniqueEventList.asUnmodifiableObservableList().toString(), uniqueEventList.toString());
+    }
+}
