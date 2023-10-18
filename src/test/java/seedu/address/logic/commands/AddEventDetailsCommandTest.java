d994572bbeda8ebdb72d52a6c6b18eaa2c3702c9
ktzy0305 <kevintoh0305@gmail.com>
2023-10-18 14:33:23 +0800

    Unit Test: AddEventDetailsCommandTest and AddEventDetailsCommandParserTest

---
+++ src/test/java/seedu/address/logic/commands/AddEventDetailsCommandTest.java
@@ -0,0 +1,55 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EVENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
+import java.util.HashSet;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.event.Event;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.EventBuilder;
+
+public class AddEventDetailsCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_allFieldsSpecified_success() {
+        HashSet<Index> personIndexes = new HashSet<>();
+        personIndexes.add(INDEX_SECOND_PERSON);
+
+        // Command to simulate
+        AddEventDetailsCommand addEventDetailsCommand = new AddEventDetailsCommand(INDEX_FIRST_EVENT, personIndexes);
+
+        // Create an edited model
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+        // Simulate adding a person into an event by swapping the existing event object with the updated event object
+        Event eventToEdit = expectedModel.getAddressBook().getEventList().get(INDEX_FIRST_EVENT.getZeroBased());
+
+        // Get the second person
+        Person person = expectedModel.getAddressBook().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+
+        // Edited event
+        Event editedEvent = new EventBuilder(eventToEdit).withPersons(List.of(person)).build();
+
+        // Update the edited event
+        expectedModel.setEvent(eventToEdit, editedEvent);
+
+        // Expected success message
+        String expectedSuccessMessage =
+                String.format(AddEventDetailsCommand.MESSAGE_SUCCESS, INDEX_FIRST_EVENT.getOneBased(),
+                        editedEvent.getName(), person.getName());
+
+        // Check if the updated model
+        assertCommandSuccess(addEventDetailsCommand, model, expectedSuccessMessage, expectedModel);
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/AddEventDetailsCommandParserTest.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_EVENT;
+
+import java.util.HashSet;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AddEventDetailsCommand;
+
+public class AddEventDetailsCommandParserTest {
+
+    private static final String MESSAGE_INVALID_FORMAT =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddEventDetailsCommand.MESSAGE_USAGE);
+
+    private final AddEventDetailsCommandParser parser = new AddEventDetailsCommandParser();
+
+    @Test
+    public void parse_missingParts_failure() {
+        // no index and no field specified
+        assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_allFieldsSpecified_success() {
+        String userInput = String.format(" %s%d %s%d",
+                PREFIX_EVENT_ID, INDEX_THIRD_EVENT.getOneBased(),
+                PREFIX_PERSON, INDEX_SECOND_PERSON.getOneBased());
+        System.out.println(userInput);
+        HashSet<Index> expectedPersons = new HashSet<>();
+        expectedPersons.add(INDEX_SECOND_PERSON);
+        AddEventDetailsCommand expectedCommand =
+                new AddEventDetailsCommand(INDEX_THIRD_EVENT, expectedPersons);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+}
