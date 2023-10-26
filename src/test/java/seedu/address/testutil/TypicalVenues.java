3bafe8419e7183e469f91fd82fc1ad1c1a01a906
ktzy0305 <kevintoh0305@gmail.com>
2023-10-26 18:24:29 +0800

    add VenueBuilder class and typical venues

--- src/test/java/seedu/address/testutil/TypicalVenues.java
+++ src/test/java/seedu/address/testutil/TypicalVenues.java
@@ -1,2 +1,49 @@
-package seedu.address.testutil;public class TypicalVenues {
+package seedu.address.testutil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.venue.Venue;
+
+/**
+ * A utility class containing a list of {@code Venue} objects to be used in tests.
+ */
+public class TypicalVenues {
+
+    public static final Venue LT27 = new VenueBuilder()
+            .withName("LT27")
+            .withAddress("Lecture Theatre 27, 10 Lower Kent Ridge Rd, Kent Ridge Campus, Singapore 119076")
+            .withCapacity("400")
+            .build();
+
+    public static final Venue CLB = new VenueBuilder()
+            .withName("CLB")
+            .withAddress("Central Library, 12 Kent Ridge Crescent, Singapore 119275")
+            .withCapacity("1000")
+            .build();
+
+    public static final Venue ECUBE = new VenueBuilder()
+            .withName("ECube")
+            .withAddress("4 Engineering Drive 3, Singapore 117583")
+            .withCapacity("100")
+            .build();
+
+    private TypicalVenues() {} // prevents instantiation
+
+    /**
+     * Returns an {@code AddressBook} with all the typical venues.
+     */
+    public static AddressBook getTypicalAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Venue venue : getTypicalVenues()) {
+            ab.addVenue(venue);
+        }
+        return ab;
+    }
+
+    public static List<Venue> getTypicalVenues() {
+        return new ArrayList<>(Arrays.asList(LT27, CLB, ECUBE));
+    }
 }
--- src/test/java/seedu/address/testutil/VenueBuilder.java
+++ src/test/java/seedu/address/testutil/VenueBuilder.java
@@ -1,2 +1,67 @@
-package seedu.address.testutil;public class VenueBuilder {
+package seedu.address.testutil;
+
+import seedu.address.model.venue.Address;
+import seedu.address.model.venue.Capacity;
+import seedu.address.model.venue.Name;
+import seedu.address.model.venue.Venue;
+
+/**
+ * A utility class to help with building Venue objects.
+ */
+public class VenueBuilder {
+    public static final String DEFAULT_NAME = "I3AUD";
+    public static final String DEFAULT_ADDRESS = "i3 Auditorium, 21 Heng Mui Keng Terrace, "
+            + "Icube Building, Singapore 119613";
+    public static final String DEFAULT_CAPACITY = "500";
+
+    private Name name;
+    private Address address;
+    private Capacity capacity;
+
+    /**
+     * Creates a {@code VenueBuilder} with the default details.
+     */
+    public VenueBuilder() {
+        name = new Name(DEFAULT_NAME);
+        address = new Address(DEFAULT_ADDRESS);
+        capacity = new Capacity(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * Initializes the VenueBuilder with the data of {@code venueToCopy}.
+     */
+    public VenueBuilder(Venue venueToCopy) {
+        name = venueToCopy.getName();
+        address = venueToCopy.getAddress();
+        capacity = venueToCopy.getCapacity();
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Venue} that we are building.
+     */
+    public VenueBuilder withName(String name) {
+        this.name = new Name(name);
+        return this;
+    }
+
+
+    /**
+     * Sets the {@code Address} of the {@code Venue} that we are building.
+     */
+    public VenueBuilder withAddress(String address) {
+        this.address = new Address(address);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Capacity} of the {@code Venue} that we are building.
+     */
+    public VenueBuilder withCapacity(String capacity) {
+        this.capacity = new Capacity(capacity);
+        return this;
+    }
+
+    public Venue build() {
+        return new Venue(name, address, capacity);
+    }
 }
