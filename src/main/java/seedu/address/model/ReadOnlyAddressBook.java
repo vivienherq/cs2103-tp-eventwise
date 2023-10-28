7477e392ec0dcbd9b367bdc09d240c003e417aeb
vivienherq <heruoqingvivien@gmail.com>
2023-10-28 16:05:15 +0800

    Update implementation of CreateVendorCommand

--- src/main/java/seedu/address/model/ReadOnlyAddressBook.java
+++ src/main/java/seedu/address/model/ReadOnlyAddressBook.java
@@ -3,6 +3,7 @@ package seedu.address.model;
 import javafx.collections.ObservableList;
 import seedu.address.model.event.Event;
 import seedu.address.model.person.Person;
+import seedu.address.model.vendor.Vendor;
 import seedu.address.model.venue.Venue;
 
 /**
@@ -33,4 +34,10 @@ public interface ReadOnlyAddressBook {
      * This list will not contain any duplicate persons.
      */
     ObservableList<Person> getEventAttendeesList();
+
+    /**
+     * Returns an unmodifiable view of the vendors list.
+     * This list will not contain any duplicate venues.
+     */
+    ObservableList<Vendor> getVendorList();
 }
