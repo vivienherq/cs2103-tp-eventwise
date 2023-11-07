c5245e6720fb0e1da0069cd48697a49ce048bea9
Dominic Ng Jun Yao <e0958779@u.nus.edu>
2023-11-07 14:08:32 +0800

    Add unit test for clear commands

--- src/test/java/seedu/address/logic/commands/ClearEventsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearEventsCommandTest.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalEvents.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,14 +10,13 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
-public class ClearCommandTest {
-
+public class ClearEventsCommandTest {
     @Test
     public void execute_emptyAddressBook_success() {
         Model model = new ModelManager();
         Model expectedModel = new ModelManager();
 
-        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ClearEventsCommand(), model, ClearEventsCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
@@ -26,7 +25,6 @@ public class ClearCommandTest {
         Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel.setAddressBook(new AddressBook());
 
-        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ClearEventsCommand(), model, ClearEventsCommand.MESSAGE_SUCCESS, expectedModel);
     }
-
 }
