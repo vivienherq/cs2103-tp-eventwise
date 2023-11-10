3a58381812035472b48e304965ca47bcd6e83ea8
Dominic Ng Jun Yao <e0958779@u.nus.edu>
2023-11-11 00:26:35 +0800

    Add unit test for ModelManager and LogicManager

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -1,13 +1,15 @@
 package seedu.address.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalEvents.ACADEMIC;
 import static seedu.address.testutil.TypicalEvents.FSC;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalRsvps.ALICE_FSC_CC;
+import static seedu.address.testutil.TypicalVendors.SUN;
+import static seedu.address.testutil.TypicalVenues.LT27;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,7 +18,10 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.person.*;
+import seedu.address.model.rsvp.Rsvp;
+import seedu.address.model.rsvp.RsvpStatus;
 import seedu.address.testutil.AddressBookBuilder;
 
 public class ModelManagerTest {
@@ -90,8 +95,80 @@ public class ModelManagerTest {
     }
 
     @Test
+    public void hasVenue_venueInAddressBook_returnsTrue() {
+        modelManager.addVenue(LT27);
+        assertTrue(modelManager.hasVenue(LT27));
+    }
+
+    @Test
+    public void hasVendor_vendorInAddressBook_returnsTrue() {
+        modelManager.addVendor(SUN);
+        assertTrue(modelManager.hasVendor(SUN));
+    }
+
+    @Test
+    public void createRsvp_invalidEventPersonIndex_returnsNull() {
+        modelManager = new ModelManager();
+        modelManager.addPerson(ALICE);
+        Index zeroIndex = Index.fromZeroBased(0);
+        Index oneIndex = Index.fromZeroBased(1);
+        Rsvp rsvp = modelManager.createRsvp(zeroIndex, zeroIndex, RsvpStatus.CC);
+        assertNull(rsvp);
+
+        modelManager.addEvent(ACADEMIC);
+        rsvp = modelManager.createRsvp(zeroIndex, oneIndex, RsvpStatus.CC);
+        assertNull(rsvp);
+    }
+
+    @Test
+    public void createRsvp_valid_returnsRsvp() {
+        modelManager = new ModelManager();
+        modelManager.addPerson(ALICE);
+        modelManager.addEvent(ACADEMIC);
+
+        Index zeroIndex = Index.fromZeroBased(0);
+
+        Rsvp modelManagerRsvp = modelManager.createRsvp(zeroIndex, zeroIndex, RsvpStatus.CC);
+        Rsvp validRsvp = new Rsvp(ACADEMIC, ALICE, RsvpStatus.CC);
+        assertEquals(modelManagerRsvp, validRsvp);
+    }
+
+    @Test
+    public void invalidRsvp_returnsFalse() {
+        Person newPersonInNoEvent = new Person(new Name("Dom"), new Phone("9123"), new Email("dom@gmail.com"));
+        Rsvp invalidRsvp = new Rsvp(ACADEMIC, newPersonInNoEvent, RsvpStatus.CC);
+        assertFalse(modelManager.isValidRsvp(invalidRsvp));
+    }
+
+    @Test
+    public void validRsvp_returnsTrue() {
+        assertFalse(modelManager.isValidRsvp(ALICE_FSC_CC));
+    }
+
+
+    @Test
     public void getFilteredPersonList_modifyList_throwsUnsupportedOperationException() {
         assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredPersonList().remove(0));
+    }
+
+    @Test
+    public void getFilteredEventAttendeesList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredEventAttendeesList().remove(0));
+    }
+
+    @Test
+    public void getFilteredEventVendorsList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredEventVendorsList().remove(0));
+    }
+
+    @Test
+    public void getFilteredDisplayableItemList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredDisplayableItemList().remove(0));
+    }
+
+    @Test
+    public void getFilteredEventRsvpList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredEventRsvpList().remove(0));
     }
 
     @Test
